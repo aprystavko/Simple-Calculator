b323c8c996c1ea7186397aa780487fc83b3e70ac
Alexey Prystavko <16720240+aprystavko@users.noreply.github.com>
2018-09-05 11:53:24 +0300

    Delete Subtraction.java

--- arithmeticOperations/Subtraction.java
+++
@@ -1,7 +0,0 @@
-package arithmeticOperations;
-
-public class Subtraction {
-    public static double subtraction(double numberOne, double numberTwo) {
-        return (numberOne - numberTwo);
-    }
-}
