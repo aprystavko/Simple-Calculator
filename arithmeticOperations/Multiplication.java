3b6d850e8644008c70097feb12dcc77345ca8046
Alexey Prystavko <16720240+aprystavko@users.noreply.github.com>
2018-09-05 11:53:18 +0300

    Delete Multiplication.java

--- arithmeticOperations/Multiplication.java
+++
@@ -1,7 +0,0 @@
-package arithmeticOperations;
-
-public class Multiplication {
-    public static double multiplication (double numberOne, double numberTwo) {
-        return numberOne * numberTwo;
-    }
-}
